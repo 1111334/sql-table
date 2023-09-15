a6f68177a3c50a8a2da3f3b8750f7f4b4fd230bb
Mike <109431533+1111334@users.noreply.github.com>
2023-09-15 12:48:29 +0200

    aggiornato

--- src/main/java/org/example/MyJDBC.java
+++ src/main/java/org/example/MyJDBC.java
@@ -13,15 +13,6 @@ public class MyJDBC {
 
             ResultSet resultSet = statement.executeQuery("SELECT * FROM `table-sql`");
 
-            while (resultSet.next()) {
-                String studentId = resultSet.getString("student_id");
-                String lastName = resultSet.getString("last_name");
-                String firstName = resultSet.getString("first_name");
-
-                String resultRow = "Student ID: " + studentId + ", Last Name: " + lastName + ", First Name: " + firstName;
-                System.out.println(resultRow);
-            }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
